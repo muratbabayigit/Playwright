f04bdaf86218d033e9c392b4283af76fb22bf03a
Babayiğit <babayigit@gmail.com>
2024-05-10 22:14:29 +0300

    first commit

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,13 @@
+import {test,expect} from '@playwright/test';
+
+test("TestOtomasyonu.com", async ({page}) =>{
+       await page.goto('https://www.testotomasyonu.com');
+       await expect(page).toHaveTitle(/Test/);
+       // toHaveTitle komutunda title'ın bir parçayı içerip içermediğine bakılıyorsa (/test/) şeklinde yazılır
+       // eğer birbir aynısı mı diye bakılacaksa 'Test Otomasyonu - Test Otomasyonu' şeklinde yazılmalıdır
+       await expect(page).toHaveURL('https://www.testotomasyonu.com')
+}
+
+);
+
+
